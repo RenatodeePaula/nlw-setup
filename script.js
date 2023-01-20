32fc234596b8e9e0a77738bba8e08e4d695a4c42
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-01-20 16:36:19 -0300

    add funções em JS

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-06"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+button.addEventListener("click", add)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  alert("Adicionado com sucesso📌")
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
+// const data = {
+//   run: ["01-01", "01-02", "01-03", "01-06", "01-07", "01-08", "01-06"],
+//   takePills: ["01-03"],
+//   journal: ["01-02", "01-12"],
+// }
 
-nlwSetup.load()
+// nlwSetup.setData(data)
+
+// nlwSetup.load()
